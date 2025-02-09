4b97cb94e06a8e5a7a4db75a4a6d9a03c894a321
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2025-02-09 21:30:07 +0600

    add: create navigation links constant for improved routing

---
+++ src/constants/index.ts
@@ -0,0 +1,27 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: "nav.about",
+    href: "#about",
+  },
+  {
+    id: 1,
+    name: "nav.status",
+    href: "#status",
+  },
+  {
+    id: 1,
+    name: "nav.resume",
+    href: "#resume",
+  },
+  {
+    id: 1,
+    name: "nav.works",
+    href: "#works",
+  },
+  {
+    id: 1,
+    name: "nav.contact",
+    href: "#contact",
+  },
+];
