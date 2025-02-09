2aac5ab871ad2a27eaa89aa13620bc5fc2562ffe
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2025-02-09 16:48:43 +0600

    add: implement Navbar component in Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Navbar from "@/components/shared/Navbar/Navbar";
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="max-w-7xl mx-auto">
+      <header>
+        <Navbar />
+      </header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
 };
 
 export default Home;
