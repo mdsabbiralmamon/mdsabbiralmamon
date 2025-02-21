0fd79878efe341c660db11be95a3bf15b9d6de44
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2025-02-21 06:02:54 +0600

    update: add project details to constants for display in Projects section

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -64,3 +64,119 @@ export const calculateSizes = (
       : [-13, -13, -10],
   };
 };
+
+export const myProjects = [
+  {
+    title: "Studio Orange - An Architecture Firm Website",
+    desc: "Studio Orange is a modern architecture firm that specializes in creating sustainable and innovative designs. The website showcases the firm's portfolio, services, and team, providing a comprehensive overview of their work.",
+    subdesc:
+      "Built with Next.js, TailwindCSS, and Framer Motion, Studio Orange's website is designed to be visually appealing and user-friendly, with a focus on responsive design and smooth animations.",
+    href: "https://studioorangearchitects.com",
+    texture: "/textures/project/project1.mp4",
+    logo: "/assets/project-logo1.png",
+    logoStyle: {
+      backgroundColor: "#2A1816",
+      border: "0.2px solid #36201D",
+      boxShadow: "0px 0px 60px 0px #AA3C304D",
+    },
+    spotlight: "/assets/spotlight1.png",
+    tags: [
+      {
+        id: 1,
+        name: "Next.js",
+        path: "/assets/nextjs.png",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: "North Studio - Architect Farm Website",
+    desc: "North Studio is a leading architecture firm that specializes in creating sustainable and innovative designs. The website showcases the firm's portfolio, services, and team, providing a comprehensive overview of their work.",
+    subdesc:
+      "Built with Next.js, TailwindCSS, and Framer Motion, North Studio's website is designed to be visually appealing and user-friendly, with a focus on responsive design and smooth animations.",
+    href: "https://northstudiobd.com",
+    texture: "/textures/project/project2.mp4",
+    logo: "/assets/project-logo2.png",
+    logoStyle: {
+      backgroundColor: "#13202F",
+      border: "0.2px solid #17293E",
+      boxShadow: "0px 0px 60px 0px #2F6DB54D",
+    },
+    spotlight: "/assets/spotlight2.png",
+    tags: [
+      {
+        id: 1,
+        name: "Next.js",
+        path: "/assets/nextjs.png",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: "Soul Connect - Matrimony Platform",
+    desc: "Soul Connect is a modern matrimony platform that connects individuals looking for a life partner. The platform offers a range of features, including profile creation, matching algorithms, with separate admin and user dashboards.",
+    subdesc:
+      "Built with React.js, TailwindCSS, and Framer Motion, Soul Connect is designed to be visually appealing and user-friendly, with a focus on responsive design and smooth animations.",
+    href: "https://soul-connect-bysam.web.app/",
+    texture: "/textures/project/project3.mp4",
+    logo: "/assets/project-logo3.png",
+    logoStyle: {
+      backgroundColor: "#60f5a1",
+      background:
+        "linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)",
+      border: "0.2px solid rgba(208, 213, 221, 1)",
+      boxShadow: "0px 0px 60px 0px rgba(35, 131, 96, 0.3)",
+    },
+    spotlight: "/assets/spotlight3.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+];
