d07f3efc7e4380cefb72985a2a701a0d2b7ddcbf
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2025-02-26 04:44:02 +0600

    feat: add border and rounded corners to project cards for improved styling

--- src/components/sections/Projects/Projects.tsx
+++ src/components/sections/Projects/Projects.tsx
@@ -41,7 +41,7 @@ const Projects = () => {
       </p>
 
       <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
-        <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
+        <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200 border border-[#3a3a3a] rounded-lg">
           <div className="absolute top-0 right-0">
             <img
               src={currentProject.spotlight}
